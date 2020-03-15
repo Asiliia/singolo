fd450b12b0306bc4d55dec88c2465de96b1c2bab
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-03-15 04:16:28 +0300

    feat: dom task, on/off phones

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const MENU = document.getElementById('navigation');
 const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
-const TOP = MENU.offsetTop;
+const SLIDER = document.getElementById('image__wrap');
+const H_PHONE = document.getElementById('hor__phone');
+const H_PHONE_LINK = "./assets/images/features/phone-horizontal.png";
+const H_PHONE_LINK_BLACK = "./assets/images/features/phone-horizontal-black.png";
+const V_PHONE = document.getElementById('vert__phone');
+const V_PHONE_LINK = "./assets/images/features/phone-vertical.png";
+const V_PHONE_LINK_BLACK = "./assets/images/features/phone-vertical-black.png";
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('navigation__selected'));
     event.target.classList.add('navigation__selected');
-    document.getElementById(event.target.classList[0].toString()).scrollIntoView(false);
-
+    let sectionId = document.getElementById(event.target.classList[0].toString());
+    if(sectionId) { 
+        sectionId.scrollIntoView();
+    }
 });
 
+H_PHONE.addEventListener('click', (event) => {
+    if (event.target.classList.contains("off")) {
+        H_PHONE.src = H_PHONE_LINK;
+        event.target.classList.remove('off');
+    } else {
+        H_PHONE.src = H_PHONE_LINK_BLACK;
+        event.target.classList.add('off');
+    }    
+});
+
+V_PHONE.addEventListener('click', (event) => {
+    if (event.target.classList.contains("off")) {
+        V_PHONE.src = V_PHONE_LINK;
+        event.target.classList.remove('off');
+    } else {
+        V_PHONE.src = V_PHONE_LINK_BLACK;
+        event.target.classList.add('off');
+    }    
+});
 
 /* BUTTON.addEventListener('click', () => {
     const subject = document.getElementById('subject').value.toString();

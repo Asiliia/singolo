6c6ec880cc8b531e3edb98c44ea6f9dc1b3b3307
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-03-16 22:25:56 +0300

    feat: dom task: switch slider

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const MENU = document.getElementById('navigation');
 const SECTIONS = document.querySelectorAll('section');
-const BUTTON = document.getElementById('form_submit');
+
 const CLOSE_BUTTON = document.getElementById('close-btn');
-//const SLIDER = document.getElementById('image__wrap');
+const SLIDER_RIGHT = document.getElementById('slides__button-right');
+const SLIDER_LEFT = document.getElementById('slides__button-left');
+
+const PHONES = document.getElementById('slide__one js');
 const H_PHONE = document.getElementById('hor__phone');
 const H_PHONE_LINK = "./assets/images/features/phone-horizontal.png";
 const H_PHONE_LINK_BLACK = "./assets/images/features/phone-horizontal-black.png";
 const V_PHONE = document.getElementById('vert__phone');
 const V_PHONE_LINK = "./assets/images/features/phone-vertical.png";
 const V_PHONE_LINK_BLACK = "./assets/images/features/phone-vertical-black.png";
+
 const SETTINGS = document.getElementById('settings');
 const PORTFOLIO_IMGS = document.getElementById('images__wrap');
 const INPUT = document.getElementById('inrut__wrap');
+const BUTTON = document.getElementById('form_submit');
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('navigation__selected'));
@@ -29,25 +34,48 @@ window.addEventListener('scroll', () => {
     MENU.querySelectorAll('a')[index].classList.add('navigation__selected');
 });
 
-H_PHONE.addEventListener('click', (event) => {
+PHONES.addEventListener('click', (event) => {
     if (event.target.classList.contains("off")) {
-        H_PHONE.src = H_PHONE_LINK;
+        event.target.src =  event.target.classList.contains("horizontal") ? H_PHONE_LINK : V_PHONE_LINK;
         event.target.classList.remove('off');
     } else {
-        H_PHONE.src = H_PHONE_LINK_BLACK;
+        event.target.src =  event.target.classList.contains("horizontal") ? H_PHONE_LINK_BLACK : V_PHONE_LINK_BLACK;
         event.target.classList.add('off');
     }    
 });
 
-V_PHONE.addEventListener('click', (event) => {
-    if (event.target.classList.contains("off")) {
-        V_PHONE.src = V_PHONE_LINK;
-        event.target.classList.remove('off');
-    } else {
-        V_PHONE.src = V_PHONE_LINK_BLACK;
-        event.target.classList.add('off');
-    }    
+let switchSlider = () => {
+    document.getElementById('inrut__wrap');
+    
+    let pointer = 0;
+    let figures = document.getElementsByTagName('figure');
+    for (let i = 0; i < figures.length; i++) {
+        if (figures[i].classList.contains('visible')) {
+            figures[i].classList.add('hidden');
+            figures[i].classList.remove('visible');
+            pointer = i;
+        }
+    }
+    if (++pointer === figures.length) {
+        pointer = 0;
+    }
+    
+    let isBlue = figures[pointer].classList.contains('blue');
+    SLIDER_LEFT.src = isBlue ? './assets/images/features/left-blue.png' : './assets/images/features/left.png';
+    SLIDER_RIGHT.src = isBlue ? './assets/images/features/right-blue.png' : './assets/images/features/right.png'; 
+    document.getElementById('main').style.backgroundColor = isBlue ? "#648BF0" :  "#f06c64";
+    figures[pointer].classList.remove('hidden');
+    figures[pointer].classList.add('visible'); 
+};
+
+SLIDER_RIGHT.addEventListener('click', () => {
+   switchSlider(); 
 });
+
+SLIDER_LEFT.addEventListener('click', () => {
+    switchSlider();
+});
+
 
 SETTINGS.addEventListener('click', (event) => {
     SETTINGS.querySelectorAll('li').forEach(el => el.classList.remove('active'));
@@ -74,10 +102,12 @@ BUTTON.addEventListener('click', () => {
         document.getElementById('msg_subject').innerText = subject ? subject : "Without subject";
         document.getElementById('msg_description').innerText = description ? description : "Without description";
         document.getElementById('message-block').classList.remove('hidden');
+        document.body.style.overflow = 'hidden';
     }
 });
 
 CLOSE_BUTTON.addEventListener('click', (event) => {
+    document.body.style.overflow = 'unset';
     MENU.querySelectorAll('result').innerText = '';
     document.getElementById('message-block').classList.add('hidden');
 });
